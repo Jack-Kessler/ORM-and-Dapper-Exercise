5f71278a4e0caadcb866c5a3e90d281b8edd9ec2
Jack Kessler <jack_kessler@hotmail.com>
2023-02-27 15:20:13 -0600

    Exercise 2 and Bonus 1

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -17,16 +17,47 @@ namespace ORM_Dapper
 
             IDbConnection conn = new MySqlConnection(connString);
 
-            var myDepartmentRepo = new DapperDepartmentRepository(conn);
+            #region Department Section
+            //var myDepartmentRepo = new DapperDepartmentRepository(conn);
 
-            myDepartmentRepo.InsertDepartment("Jacks New Department");
+            //myDepartmentRepo.InsertDepartment("Jacks New Department");
 
-            var departments = myDepartmentRepo.GetAllDepartments();
+            //var departments = myDepartmentRepo.GetAllDepartments();
 
-            foreach (var department in departments)
+            //foreach (var department in departments)
+            //{
+            //    Console.WriteLine(department.DepartmentID);
+            //    Console.WriteLine(department.Name);
+            //    Console.WriteLine();
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            var myProductRepository = new DapperProductRepository(conn);
+
+            //myProductRepository.CreateProduct("Js New Product", 77.77, 9);
+
+            var myProductToUpdate = myProductRepository.GetProductById(942); //We creaeted a new product in SQL Workbench directly with product ID = 942
+
+            //What we want to change with product (Below)
+            myProductToUpdate.Name = "UPDATE TEST";
+            myProductToUpdate.Price = 99.99;
+            myProductToUpdate.CategoryID = 1;
+            myProductToUpdate.OnSale = false;
+            myProductToUpdate.StockLevel = 99;
+
+            myProductRepository.UpdateProduct(myProductToUpdate);
+
+            var products = myProductRepository.GetAllProducts();
+
+            foreach (var product in products)
             {
-                Console.WriteLine(department.DepartmentID);
-                Console.WriteLine(department.Name);
+                Console.WriteLine(product.ProductID);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.CategoryID);
+                Console.WriteLine(product.OnSale);
+                Console.WriteLine(product.StockLevel);
                 Console.WriteLine();
                 Console.WriteLine();
             }
