424ef21f9c0b8f09feb055ecb736bb85e67c7fc2
Jack Kessler <jack_kessler@hotmail.com>
2023-02-27 14:10:47 -0600

    Exercise 1

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    internal class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperDepartmentRepository(IDbConnection conn) //Custom Constructor
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _conn.Query<Department>("SELECT * FROM Departments");
+        }
+        public void InsertDepartment(string name)
+        {
+            _conn.Execute("INSERT INTO Departments (Name) VALUES (@name)", new { name = name });
+        }
+    }
+}
